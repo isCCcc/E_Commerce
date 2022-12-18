1b1a95a31b8074d250ae0514ef0b77d16872a626
1935158902@qq.com <1935158902@qq.com>
2022-12-18 21:27:16 +0800

    完成分类页面的页面布局和样式渲染

--- pages/category/index.js
+++ pages/category/index.js
@@ -6,6 +6,8 @@ Page({
     leftMenuList: [],
     // 右侧商品数据
     rightContent: [],
+    // 被点击的菜单索引
+    currentIndex: 0,
   },
   Cates: [],
   onLoad() {
@@ -17,7 +19,7 @@ Page({
         this.Cates = result.data.message
         console.log(this.Cates)
         let leftMenuList = this.Cates.map(data => data.cat_name)
-        let rightContent = this.Cates.map(data => data.children)
+        let rightContent = this.Cates[0].children
         this.setData({ leftMenuList, rightContent })
       })
   }
