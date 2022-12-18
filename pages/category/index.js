247759f71af4e3b9d84fc473482e455fd885d78a
1935158902@qq.com <1935158902@qq.com>
2022-12-18 23:00:52 +0800

    分类页面数据请求添加缓存-5分钟缓存失效

--- pages/category/index.js
+++ pages/category/index.js
@@ -13,12 +13,30 @@ Page({
   },
   Cates: [],
   onLoad() {
-    this.getCates()
+    let Cates = wx.getStorageSync('cates')
+    if (!Cates) {
+      this.getCates()
+    } else {
+      // 设置5分钟缓存失效
+      if (Date.now - Cates.time > 1000 * 60 * 5) {
+        this.getCates()  // 重新发送请求
+      } else {
+        // 使用旧数据
+        this.Cates = Cates.data
+        let leftMenuList = this.Cates.map(data => data.cat_name)
+        let rightContent = this.Cates[0].children
+        this.setData({ leftMenuList, rightContent })
+      }
+    }
   },
   getCates() {
     request({ url: "/categories" })
       .then(result => {
         this.Cates = result.data.message
+
+        // 将接口数据存入本地存储
+        wx.setStorageSync('cates', { time: Date.now(), data: this.Cates })
+
         let leftMenuList = this.Cates.map(data => data.cat_name)
         let rightContent = this.Cates[0].children
         this.setData({ leftMenuList, rightContent })
