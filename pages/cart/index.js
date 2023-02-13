345abf9a941f0588b8ab588cb75f507f89b1b1c1
1935158902@qq.com <1935158902@qq.com>
2023-02-13 17:10:39 +0800

    完成：购物车 - 获取收货地址

--- pages/cart/index.js
+++ pages/cart/index.js
@@ -15,18 +15,23 @@ Page({
 
     // 新增收获地址
     handleChoose() {
+        // 1.获取权限状态
         wx.getSetting({
-            success:(result)=>{
+            success: (result) => {
+                // 2.判断权限状态
                 const scopeAddress = result.authSetting["scope.address"]
-                if(scopeAddress===true||scopeAddress===undefined){
+                if (scopeAddress === true || scopeAddress === undefined) {
+                    // 3.调用获取收货地址的 api
                     wx.chooseAddress({
                         success: (result1) => {
                             console.log(result1);
                         }
                     })
-                }else {
+                } else {
+                    // 4.诱导用户打开授权页面
                     wx.openSetting({
-                        success:(result2)=>{
+                        success: (result2) => {
+                            // 3.调用获取收货地址的 api
                             wx.chooseAddress({
                                 success: (result3) => {
                                     console.log(result3);
