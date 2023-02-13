23c089ed54cb30edec67ddd4f7572be23eef8008
1935158902@qq.com <1935158902@qq.com>
2023-02-13 16:59:16 +0800

    完成：购物车 - 获取收货地址

--- pages/cart/index.js
+++ pages/cart/index.js
@@ -1,66 +1,41 @@
 // pages/cart/index.js
 Page({
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
+    /**
+     * 页面的初始数据
+     */
+    data: {},
 
-  },
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad(options) {
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad(options) {
+    },
 
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload() {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {
-
-  }
+    // 新增收获地址
+    handleChoose() {
+        wx.getSetting({
+            success:(result)=>{
+                const scopeAddress = result.authSetting["scope.address"]
+                if(scopeAddress===true||scopeAddress===undefined){
+                    wx.chooseAddress({
+                        success: (result1) => {
+                            console.log(result1);
+                        }
+                    })
+                }else {
+                    wx.openSetting({
+                        success:(result2)=>{
+                            wx.chooseAddress({
+                                success: (result3) => {
+                                    console.log(result3);
+                                }
+                            })
+                        }
+                    })
+                }
+            }
+        })
+    }
 })
