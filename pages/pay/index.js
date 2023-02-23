d28b53fc5719fdd237c1a03479ae23f0f716c04e
1935158902@qq.com <1935158902@qq.com>
2023-02-23 11:20:17 +0800

    完成：购物车 - 商品支付页面的渲染&显示逻辑处理

--- pages/pay/index.js
+++ pages/pay/index.js
@@ -1,66 +1,27 @@
 // pages/pay/index.js
+import {showToast} from "../../utils/asyncWx";
+import regeneratorRuntime from "../../lib/runtime/runtime"
+
 Page({
+    data: {
+        address: {},
+        cart: [],
+        totalPrice: 0,
+        totalNum: 0
+    },
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad(options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload() {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {
-
-  }
+    onShow() {
+        const address = wx.getStorageSync("address")
+        let cart = wx.getStorageSync("cart")
+        //计算全选：注意空数组调用every时为true
+        // const allChecked = cart.length ? cart.every(v => v.checked) : false;
+        this.setData({address})
+        let totalPrice = 0, totalNum = 0
+        cart = cart.filter(c => c.checked)
+        cart.forEach(item => {
+            totalPrice += item.goods_price * item.num
+            totalNum += item.num
+        })
+        this.setData({cart, totalPrice, totalNum})
+    }
 })
